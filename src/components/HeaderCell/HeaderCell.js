db3026d5290be7845f7b1dc92bada005ae2a0ef9
VB <vladimir@acclaim.consulting>
2019-03-10 16:15:10 -0700

    fix: time indicator positioning hack

--- src/components/HeaderCell/HeaderCell.js
+++ src/components/HeaderCell/HeaderCell.js
@@ -15,11 +15,11 @@ export const DayCell = ({date, currentTime}) => {
     const hours = currentTime ? currentTime.getHours() : null;
     const min = currentTime ? currentTime.getMinutes() : null;
     const needDivider = (hours>=date && hours<date + 1);
-    const offset =  config.cellHeight * min/60;
+    const offset =  Math.ceil(config.cellHeight * min/60) - 6; // HACK 
     return (
     <div className="day_cell" style={{ height: config.cellHeight, position:'relative'}}>
+        {needDivider ? <hr className='timeDiv' style={{ top: `${offset}px`}} ></hr> : null}
         <p className="time_indicator">{date}:00</p>
-        {needDivider ? <hr className='timeDiv' style={{ top: offset}} ></hr> : null}
     </div>
 
 )};
